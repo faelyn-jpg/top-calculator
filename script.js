ad440975f533a58d62ddc65fbeb0aea44327bb2e
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-10-03 15:17:53 +1300

    add display to html file

--- script.js
+++ script.js
@@ -18,4 +18,14 @@ function divide(num1, num2) {
   return num1 / num2
 }
 
-function operate(num1, num2, operator) {}
+function operate(num1, num2, operator) {
+  if (operator === add) {
+    add(num1, num2)
+  } else if (operator === subtract) {
+    subtract(num1, num2)
+  } else if (operator === multiply) {
+    multiply(num1, num2)
+  } else if (operator === divide) {
+    divide(num1, num2)
+  }
+}
