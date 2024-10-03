c9fc2bdd78511b8a612f20905b2442c1f8e3dcdf
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-10-03 13:15:15 +1300

    add basic calculator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+let num1
+let num2
+let operator
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+function operate(num1, num2, operator) {}
