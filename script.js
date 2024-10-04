efb33cdbe5347eee6ff727645015dcba797a9e70
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-10-04 16:57:15 +1300

    add oops i finished everything

--- script.js
+++ script.js
@@ -1,31 +1,86 @@
 let num1
 let num2
-let operator
+let operator = [
+  function add(num1, num2) {
+    return num1 + +num2
+  },
+  function subtract(num1, num2) {
+    return num1 - +num2
+  },
+  function multiply(num1, num2) {
+    return num1 * +num2
+  },
+  function divide(num1, num2) {
+    return num1 / +num2
+  },
+]
+let currentOperator
+let displayValue
+let display = document.querySelector('.display')
 
-function add(num1, num2) {
-  return num1 + num2
+function addToDisplay(displayValue) {
+  display.textContent = displayValue
 }
 
-function subtract(num1, num2) {
-  return num1 - num2
+function clearCalculator() {
+  displayValue = ''
+  num1 = ''
+  num2 = ''
+  currentOperator = ''
+  addToDisplay(displayValue)
 }
 
-function multiply(num1, num2) {
-  return num1 * num2
+function operate(num1, num2) {
+  let result = operator[currentOperator](num1, num2)
+  displayValue = result
+  num1 = result
+  addToDisplay(displayValue)
+  return result
 }
 
-function divide(num1, num2) {
-  return num1 / num2
-}
+let button = document.querySelectorAll('button')
+console.log(`button is ${button}`)
 
-function operate(num1, num2, operator) {
-  if (operator === add) {
-    add(num1, num2)
-  } else if (operator === subtract) {
-    subtract(num1, num2)
-  } else if (operator === multiply) {
-    multiply(num1, num2)
-  } else if (operator === divide) {
-    divide(num1, num2)
+document.addEventListener('click', (e) => {
+  if (
+    e.target.matches('.digit') ||
+    e.target.matches('.operator') ||
+    e.target.matches('#equal')
+  ) {
+    if (
+      currentOperator &&
+      !e.target.matches('#equal') &&
+      e.target.matches('.digit')
+    ) {
+      num2 ? (num2 += e.target.textContent) : (num2 = e.target.textContent)
+      console.log(`num2 is ${num2}`)
+    }
+    if (e.target.matches('.operator') && currentOperator && num2) {
+      num1 = operate(num1, num2)
+      currentOperator = e.target.id
+      console.log(`currentOperator is ${currentOperator}`)
+      num2 = ''
+      console.log(`num1 is ${num1}`)
+    } else if (e.target.matches('.operator')) {
+      num1 = +displayValue
+      currentOperator = e.target.id
+      console.log(`num1 is ${num1}`)
+      console.log(`currentOperator is ${currentOperator}`)
+    }
+    if (!currentOperator && num1) clearCalculator()
+    if (e.target.matches('#equal')) {
+      console.log('clicked!')
+      operate(num1, num2)
+      num2 = ''
+      currentOperator = ''
+    } else {
+      displayValue
+        ? (displayValue += e.target.textContent)
+        : (displayValue = e.target.textContent)
+      addToDisplay(displayValue)
+    }
   }
-}
+  if (e.target.matches('#clear')) {
+    clearCalculator()
+  }
+})
